caccbaf644029df072440cb2024a1314b68543e4
katty <katerina.mironchik@gmail.com>
2021-05-01 21:41:28 +0300

    toroidal space movement logic

--- Assets/Scripts/RocketBehaviour.cs
+++ Assets/Scripts/RocketBehaviour.cs
@@ -5,8 +5,10 @@ public class RocketBehaviour : MonoBehaviour
 {
     private readonly int maxSpeed = 5;
     private readonly float enginePower = 1;
+    private readonly float torqueVelocity = -0.1f;
 
     private Rigidbody2D rig { get; set; }
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +19,15 @@ public class RocketBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.position = Vector3.zero;
+            return;
+        }
+
         var thrust = Math.Max(Input.GetAxis("Vertical"), 0) * enginePower;
         var possibleSpeedUp = (maxSpeed - rig.velocity.magnitude) * thrust;
         rig.AddRelativeForce(new Vector2(0, Math.Max(possibleSpeedUp, 0) * rig.mass));
-        rig.AddTorque(-1 * Input.GetAxis("Horizontal"));
+        rig.AddTorque(torqueVelocity * Input.GetAxis("Horizontal"));
     }
 }
